64e964b264fabac925c619b4bd301f94efab93a3
Changbeom Choi <cbchoi@handong.edu>
2017-03-21 12:31:03 +0000

    Init

---
+++ tinyxml_test.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include "tinyxml.h"
+
+int main()
+{
+    TiXmlDocument doc( "basic.xml" );
+	bool loadOkay = doc.LoadFile();
+
+	if ( !loadOkay )
+	{
+		printf( "Could not load test file 'demotest.xml'. Error='%s'. Exiting.\n", doc.ErrorDesc() );
+		exit( 1 );
+	}
+	
+	//doc.Print(stdout);
+	
+	TiXmlNode* node = 0;
+	TiXmlElement* SESElement = 0;
+	TiXmlElement* SystemElement = 0;
+	TiXmlElement* Element = 0;
+	TiXmlElement* SubElement = 0;
+	
+	node = doc.FirstChild( "SystemEntityStructure" );
+	SESElement = node->ToElement();
+	SystemElement = SESElement->FirstChildElement();	
+	Element = SystemElement->FirstChildElement("AttributeNodeList");
+	
+	SubElement = Element->FirstChildElement("Attribute");
+	SubElement->Print(stdout, 0);
+	std::cout<<std::endl;
+
+	while(SubElement = SubElement->NextSiblingElement("Attribute"))
+	{
+		SubElement->Print(stdout, 0);
+		std::cout<<std::endl;
+	}
+	
+    return 0;
+}
